cecff8e5704dbc9a9ce9d86f0e629ea679129e42
Greg Turner <greg.fun@gmail.com>
2017-01-12 13:56:55 -0800

    basic http->https proxy

---
+++ index.js
@@ -0,0 +1,20 @@
+var https = require('https'),
+    http  = require('http'),
+    util  = require('util'),
+    path  = require('path'),
+    fs    = require('fs'),
+    colors = require('colors'),
+    httpProxy = require('http-proxy');
+
+//
+// Create a HTTP Proxy server with a HTTPS target
+//
+httpProxy.createProxyServer({
+    target: 'https://t2mspde.maximusbc.ca',
+    agent  : https.globalAgent,
+    headers: {
+        host: 't2mspde.maximusbc.ca'
+    }
+}).listen(9080);
+
+console.log('https proxy server started on port 9080'.green.bold);
