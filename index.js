dd7f15d94f0ee4237897021a35ca3a2d0fa74691
Greg Turner <greg.fun@gmail.com>
2017-02-01 16:18:33 -0800

    make secure again

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (process.env.MUTUAL_TLS_PEM_KEY_BASE64 &&
 var proxy = httpProxy.createProxyServer({
     target: process.env.TARGET_URL,
     agent  : myAgent || https.globalAgent,
-    secure: false,
+    secure: true,
     headers: {
         host: process.env.TARGET_HEADER_HOST
     },
